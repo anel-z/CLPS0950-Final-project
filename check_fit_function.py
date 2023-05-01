f12a0c2e369ca99733d41a6d13cd17a9dc1d3103
Rachel Vidomlanski <rachel_vidomlanski@brown.edu>
2023-05-01 15:12:51 -0400

    not ready yet

--- check_fit_function.py
+++ check_fit_function.py
@@ -1,21 +1,23 @@
-def check_fit(row_index, column_index, len_word, size_board):
+def check_fit(row_index, column_index, len_word, size_board, used_indices):
   directions = []
-  if len_word <= (column_index):
+  if len_word <= (column_index) and [(row_index, i) not in used_indices for i in range (column_index, column_index-len_word)]:
       directions.append('horizontal left')
   
-  if column_index + len_word <= (size_board-column_index):
+  if column_index + len_word <= (size_board-column_index) and [(row_index, i) not in used_indices for i in range (column_index, column_index + len_word)]:
     directions.append( 'horizontal right')
-    
-  if len_word <= (row_index):
+
+
+  if len_word <= (row_index) and [(i, column_index) not in used_indices for i in range (row_index, row_index-len_word)]:
      directions.append('vertical up')
    
-  if row_index + len_word <= (size_board-row_index):
+  if row_index + len_word <= (size_board-row_index) and [(i, column_index) not in used_indices for i in (row_index, row_index+len_word)]:
       directions.append('vertical down')
      
-  if len_word<= column_index and len_word<= row_index:
+  if len_word<= column_index and len_word<= row_index and [(row_index+i, column_index+i) not in used_indices for i in (0, len_word)]:
      directions.append('diagonal bottom to top')
 
-  if column_index + len_word <= (size_board-column_index) and row_index + len_word <= (size_board-row_index):
+  if column_index + len_word <= (size_board-column_index) and row_index + len_word <= (size_board-row_index) and [(row_index-i, column_index-i) not in used_indices for i in (0, len_word)]:
     directions.append('diagonal top to bottom')
-
+ 
+  return directions
   print(directions)
--- maybe_actual_board.py
+++ maybe_actual_board.py
@@ -30,15 +30,69 @@ print(individual_letters)
 used_indices = []
 searchboard = generate_board(15)
 
-for n in range (0, len(words_for_board)):
-  len_word = len(words_for_board[n])
-  size_board = len(searchboard)
-  row_index = random.randint(0, size_board-1 )
-  column_index = random.randint(0, len(searchboard[0])-1)
-  random_index = (row_index, column_index)
-  check_fit(row_index, column_index, len_word, size_board)
-  direction = random.sample(directions, 1)
+
+while len(words_for_board)> 0:
+  possible_directions = []
+  n=0
+  while possible_directions == []:
+    len_word = len(words_for_board[n])
+    size_board = len(searchboard)
+    row_index = random.randint(0, size_board-1 )
+    column_index = random.randint(0, len(searchboard[0])-1)
+    random_index = (row_index, column_index)
+    possible_directions = check_fit(row_index, column_index, len_word, size_board, used_indices)
+    print(possible_directions)
+
+
+  direction = random.sample(possible_directions, 1)
   print(direction)
+  words_for_board.pop(0)
+
+
+
+  if direction[0] == 'horizontal left':
+    print("here 1")
+    for x in range (0, len_word):
+      searchboard[row_index] [column_index - x] = individual_letters[n][x]
+      used_indices.append[[row_index] [column_index - x]]
+    words_for_board.pop(0)
+
+  elif direction[0] == 'horizontal right':
+    for x in range (0, len_word):
+     searchboard[row_index] [column_index + x] = individual_letters[n][x]
+     used_indices.append[(row_index, column_index + x)]
+    words_for_board.pop(0)
+  elif direction[0] == 'vertical up':
+    print("here 3")
+    for x in range (0, len_word):
+      searchboard[(row_index-x, column_index)] = individual_letters[n][x]
+      used_indices.append[[row_index-x] [column_index]]
+    words_for_board.pop(0)
+  elif direction[0] == 'vertical down':
+    print("here 4")
+    for x in range (0, len_word):
+      searchboard[(row_index+x, column_index)] = individual_letters[n][x]
+      used_indices.append[(row_index+x,column_index)]
+    words_for_board.pop(0)
+  elif direction[0] == 'diagonal top to bottom':
+    print("here 5")
+    for x in range (0, len_word):
+      searchboard[(row_index-x, column_index-x)] = individual_letters[n][x]
+      used_indices.append[[row_index-x] [column_index-x]]
+    words_for_board.pop(0)
+  elif direction[0] == 'diagonal top to bottom':
+    print("here 6")
+    for x in range (0, len_word):
+      searchboard[(row_index+x,column_index+x)] = individual_letters[n][x]
+      used_indices.append[[row_index+x] [column_index+x]]
+    words_for_board.pop(0)
+  
+  print(words_for_board)
+
+print(searchboard)  
+#once i replace the letters, delete words_for_board[0] from the list
+
+  
 
 
 
@@ -77,7 +131,7 @@ for n in range (0, len(words_for_board)):
 #       else:
 #          used_indices.append((column_index, row_index))
         
-#          ### need to add a pick a new index12cx and not just don't replace it 
+#          # need to add a pick a new index12cx and not just don't replace it 
 # print(used_indices)
 # print(searchboard)   
 
@@ -92,7 +146,7 @@ for n in range (0, len(words_for_board)):
 
 
 
-# #generating a random index to test 
+# generating a random index to test 
 # row_index = random.randint(0, len(searchboard) )
 # column_index = random.randint(0, len(searchboard[0]))
 # random_index = (row_index, column_index)
@@ -101,11 +155,11 @@ for n in range (0, len(words_for_board)):
 
 
 
-#used_indices = []
-#searchboard = generate_board(15)
-#for n in range (0, len(words_for_board)):
-  #for x in range (0, len(big_lists[n])):
-   # searchboard[x,n] = big_lists[n][x]
-   # used_indices.append((n,x))
-#print(used_indices)
-#print(searchboard) 
+# used_indices = []
+# searchboard = generate_board(15)
+# for n in range (0, len(words_for_board)):
+#   for x in range (0, len(big_lists[n])):
+#   searchboard[x,n] = big_lists[n][x]
+#   used_indices.append((n,x))
+# print(used_indices)
+# print(searchboard) 
