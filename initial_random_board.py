865a1f36ed7bc7fb72661022346548c7293829b8
anel-z <127125808+anel-z@users.noreply.github.com>
2023-05-03 10:26:42 -0400

    bla bla

--- initial_random_board.py
+++ initial_random_board.py
@@ -8,7 +8,7 @@ def board(int):
   for n in range (0,int):
     for x in range (0,int):
      array[x,n] = NumtoAlph(random.randint(26))
-  return array
+  return array.decode()
   
  
 print(board(15))
